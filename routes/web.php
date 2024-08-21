8b3188a610aed3db4d90ba0322c570a01d32d493
Alexandre Marques Bulcao <ambulcao@gmail.com>
2024-08-22 00:27:15 +0100

    chore: adicionando rotas
    chore: adicionando rotas no ficheiro de routes e criando users no seed.

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+
+Route::resources([
+    'users' => UserController::class,
+]);
 
 Route::get('/', function () {
     return view('welcome');
